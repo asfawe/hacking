e904e3a21ca76b690c138786399bae5b6a42c431
asfawe <aronia5137@gmail.com>
2023-06-23 20:58:35 +0900

    ...

--- myproject-crud-node/server.js
+++ myproject-crud-node/server.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const server = express();
+const app = express();
 const User = require('./models/User');
 const port = 3000;
 require('dotenv').config({path:'variables.env'}); // dotenv 파일이 어디있냐? 바로 variable.env 파일 안에 있다.
 
-server.get('/', (req, res) => {
-	const newUser = new User();
-	newUser.name = "Ecen2033";
-	newUser.email = "Ecen2033@gmail.com";
-	newUser.age = 20;
-	newUser.save()
-	.then((user) => {
-		console.log(user);
-		res.json({
-			message: 'User Created Successfully',
-		})
-	})
-	.catch((err) => {
-		res.json({
-			message: err.toString(),
-		});
-	});
+app.use(express.static('./images/'));
+
+app.get('/', function (req, res) {
+	return res.sendFile(__dirname + "/templates/index.html");
 });
 
-server.listen(port, (err) => {
+// app.get('/', (req, res) => {
+// 	const newUser = new User();
+// 	newUser.name = "Ecen2033";
+// 	newUser.email = "Ecen2033@gmail.com";
+// 	newUser.age = 20;
+// 	newUser.save()
+// 	.then((user) => {
+// 		console.log(user);
+// 		res.json({
+// 			message: 'User Created Successfully',
+// 		})
+// 	})
+// 	.catch((err) => {
+// 		res.json({
+// 			message: err.toString(),
+// 		});
+// 	});
+// });
+
+app.listen(port, (err) => {
 	if (err) {
 		return console.error(err);
 	} else {
