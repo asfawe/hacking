250260412fc6b8e7fdaba4692f7c5ebce75a02ec
asfawe <aronia5137@gmail.com>
2023-06-24 08:10:07 +0900

    comment Modify

--- myproject-crud-node/models/User.js
+++ myproject-crud-node/models/User.js
@@ -19,8 +19,8 @@ const userSchema = new Schema(
 });
 
 userSchema.pre("save", async function () {
-	if (this.isModified("password")) { // 여기서 this는 userSchema의 그러니깐 User 생성자의 인스턴스를 말한다.
-	  this.password = await bcrypt.hash(this.password, 5);
+	if (this.isModified("password")) { // 만약 비밀번호가 변경되었다면 (예를 들어, 처음 생성되었거나 사용자가 비밀번호를 업데이트한 경우), 이 미들웨어는 bcrypt를 사용하여 비밀번호를 해싱합니다.
+	  this.password = await bcrypt.hash(this.password, 5); // 이 경우 this는 userSchema에 따라 생성된 문서, 즉 하나의 'User'를 가리킵니다. 
 	}
   });
 
