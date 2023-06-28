6a38c08501f0fbba2dcba02c77764629998285c5
asfawe <aronia5137@gmail.com>
2023-06-28 11:07:24 +0900

    ...

--- myproject/myproject-crud-node-v2/router/main/main_route.js
+++ myproject/myproject-crud-node-v2/router/main/main_route.js
@@ -10,4 +10,13 @@ router.post('/', (req, res) => {
 	res.render('index', {http : "POST"});
 });
 
+router.get('/hehe', (req, res) => {
+	res.json({
+		report: "hahaha"
+	  }).status(200);
+	console.log(res.statusCode);
+	console.log(res.statusCode);
+	console.log(res.statusCode);
+});
+
 module.exports = router;
